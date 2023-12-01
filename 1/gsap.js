426fa0b7de19c1b68977f9f4eef1f545b7be03b4
rio.coder <141744999+riocoder7@users.noreply.github.com>
2023-12-01 10:05:14 +0000

    Update gsap.js
    change

--- 1/gsap.js
+++ 1/gsap.js
@@ -1,1 +1,53 @@
-gsap.to("#text h1" )
+const ti = gsap.timeline();
+
+const all_Ani= ()=>{
+    ti.from("#logo h1, #link h3, #btn",{
+        y:"-100%",
+        duration:0.5,
+        delay:1,
+        opacity:0,
+        stagger:0.3
+    
+    });
+    
+    ti.from("#text h1",{
+        y:"-100%",
+        opacity:0,
+        stagger:0.3
+    
+    })
+    
+    ti.from("#first img, #src img",{
+        y:"-100%",
+        opacity:0,
+        stagger:0.2,
+        yoyo:true,
+    
+    });
+    
+    ti.from("#scroll h2",{
+        y:40,
+        repeat:-1,
+        yoyo:true,
+    });
+    ti.to("#src img",{
+        x:60,
+        y:-100,
+        duration:5,
+        repeat:-1,
+        yoyo:true,
+    
+    })
+    
+    ti.to("#first img",{
+        x:60,
+        y:100,
+        duration:5,
+        repeat:-1,
+        yoyo:true,
+    
+    })
+};
+
+
+all_Ani();
