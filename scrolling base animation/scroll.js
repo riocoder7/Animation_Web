619d9eba1bc4cac41259a634e976dbe79f76f99f
rio.coder <141744999+riocoder7@users.noreply.github.com>
2023-12-06 10:22:16 +0000

    Update scroll.js

--- scrolling base animation/scroll.js
+++ scrolling base animation/scroll.js
@@ -3,16 +3,16 @@ const t1 = gsap.timeline();
 
 t1.from("nav #logo, nav #link",{
     y:"-150%",
-    duration:0.7,
+    duration:0.5,
     
-    stagger:0.5,
+    stagger:0.3,
     opacity:0
 
 });
 
 t1.from(" nav #link h1",{
     y:"-150%",
-    duration:0.5,
+    duration:0.3,
     
     stagger:0.5,
     opacity:0
@@ -21,9 +21,9 @@ t1.from(" nav #link h1",{
 
 t1.from(" nav #btn button",{
     y:"-150%",
-    duration:0.5,
+    duration:0.3,
     
-    stagger:0.5,
+    stagger:0.3,
     opacity:0
 
 });
@@ -45,6 +45,7 @@ t1.from("#page1 #right img",{
 
 });
 t1.to("#page1 #right img",{
+    
     y:-100,
     x:100,
     duration:1,
@@ -56,20 +57,31 @@ t1.to("#page1 #right img",{
 
 });
 
-t1.from("#page1 #two_box #one",{
+gsap.from("#page1 #two_box #one",{
     y:100,
     x:-400,
-    duration:0.5,
-    stagger:0.3,
-    opacity:0
+    duration:1,
+    stagger:1,
+    opacity:0,
+    scrollTrigger:{
+        trigger:"#page1 #two_box #one",
+        scroller:"body",
+        start:"top 60%",
+        
+        // markers:true,
+    },
+    
 
 });
     
-t1.to("#page1 #two_box #one",{
+gsap.to("#page1 #two_box #one",{
    
     scrollTrigger:{
         trigger:"#page1 #two_box #one",
-        scroller:"body"
+        scroller:"body",
+        start:"top 50%",
+        
+        // markers:true,
     },
     x:500,
     duration:2,
@@ -83,41 +95,71 @@ t1.to("#page1 #two_box #one",{
 });
 
 
-t1.from("#page1 #two_box #two",{
+gsap.from("#page1 #two_box #two",{
+    
     y:100,
     x:400,
     duration:0.5,
     stagger:0.3,
-    opacity:0
-
+    opacity:0,
+    scrollTrigger:{
+        trigger:"#page1 #two_box #two",
+        scroller:"body",
+        start:"top 50%",
+        // markers:true,
+    },
+    
 });
 
 
     
-t1.to("#page1 #two_box #two",{
+gsap.to("#page1 #two_box #two",{
+    
+    scrollTrigger:{
+        trigger:"#page1 #two_box #two",
+        scroller:"body",
+        start:"top 40%",
+        
+        // markers:true,
+    },
     x:-400,
     duration:2,
     delay:1,
     stagger:0.3,
     // rotate:360,
-    opacity:0,
+    // opacity:0,
     repeat:-1,
     yoyo:true,
+    opacity:0.2,
 
 });
 
 
-t1.from("#page2  .one, #page2 .one",{
+
+gsap.from("#page2  .one",{
+    scrollTrigger:{
+        trigger:"#page2 .one",
+        scroller:"body",
+        start:"top 40%",
+        // markers:true,
+    },
     y:-100,
     x:400,
     duration:0.5,
     stagger:0.3,
-    opacity:0
+    opacity:0,
+   
 
 });
 
 
-t1.from("#page2  .two, #page2 .two",{
+gsap.from("#page2  .two",{
+    scrollTrigger:{
+        trigger:"#page2 .two",
+        scroller:"body",
+        start:"top 40%",
+        // markers:true,
+    },
     y:100,
     x:400,
     duration:0.5,
@@ -125,3 +167,18 @@ t1.from("#page2  .two, #page2 .two",{
     opacity:0
 
 });
+
+
+gsap.from("#footer",{
+    scrollTrigger:{
+        trigger:"#footer",
+        scroller:"body",
+        start:"top 60%",
+        // markers:true,
+        color:"green",
+    },
+    y:-100,
+    duration:0.5,
+    stagger:0.3,
+    opacity:0
+})
